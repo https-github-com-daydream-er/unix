1767e2b06f45edac7e8cb64ca98723248400cac2
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2023-12-05 21:32:09 +0900

    Nothing Appear infinite loop :(

---
+++ UnixProgramming/project.h
@@ -0,0 +1,43 @@
+#ifndef PROJECT_H
+# define PROJECT_H
+
+#include<unistd.h>
+#include<errno.h>
+#include<sys/fcntl.h>
+#include<sys/types.h>
+#include<sys/fcntl.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+// 1MB = 1048576
+// 1/4 MB = 262144
+#define MB 262144
+#define IN 0
+#define OUT 1
+
+#define COMM 0
+#define COMP 1
+#define IO 2
+#define SEC 1000000
+
+// clinet.c
+void	debug_result(void);
+void	do_comm_node(int id, char *client_fifo_path, char *server_fifo_path);
+void	do_io_node(int id, char *io_fifo_path);
+
+int     server_oriented_io(); // parallel_operation 실행 및 시간 측정
+void	parallel_operation(char *client_fifo_path, char *server_fifo_path, char *io_fifo_path); // Client2Server와 parent 실행
+void    parent(char *str); // 4개의 client2Server의 부모
+void	parent(char *str); // 4개의 client2Server의 부모
+void	Client2Server(int i, int client2client[4][4][2]); // do_io_node, do_compute_node, do_comm_node
+
+int     ft_compare(const void *a, const void *b);
+void	do_compute_node(int *dump); // compute -> sort data
+void	comm_init(int id, char *fifo_path, int data[MB]); // communicate
+void    send_server(char *fifo_path, int dump[MB]); // server 전달
+void	writeTimeAdvLock(int index, int time_result);
+void Server(char *io_fifo_path);
+
+#endif
